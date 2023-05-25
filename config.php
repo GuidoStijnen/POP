ee1f146f491358964b574c12384520871392fbd4
guido stijnen <117721871+GuidoStijnen@users.noreply.github.com>
2023-05-25 11:21:57 +0200

    Add files via upload

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
-    $config = [
-        'servername' => '127.0.0.1',
-        'username' => 'proftaak',
-        'password' => 'GS2001',
-        'dbname' => 'pop'
-    ];
+$servername = "127.0.0.1:3307";
+$username = "proftaak";
+$password = "GS2001";
+$dbname = "pop";
+
+// Create a new MySQLi object
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Failed to connect to MySQL: " . $conn->connect_error);
+}
 ?>
--- pop.php
+++ pop.php
@@ -58,15 +58,15 @@
         <th>Action</th> <!-- new column for delete button -->
     </tr>
 
-</table>
+ </table>
 	</style>
-</head>
-<body>
-	<div class="container">
+ </head>
+ <body>
+	    <div class="container">
 		<h1>Welcome to my website!</h1>
 		<p>This website is a device management</p>
-    <!-- Form for adding data -->
-	<form method="post">
+        <!-- Form for adding data -->
+	 <form method="post">
 		<label for="id">ID:</label>
 		<input type="text" name="id" id="id" required>
 		<label for="name">Name:</label>
@@ -76,34 +76,41 @@
 		<label for="ipv4">IPv4:</label>
 		<input type="text" name="ipv4" id="ipv4" required>
 		<input type="submit" name="submit" value="Add Data">
-	</form>
+	 </form>
 	
-	<table>
+	 <table>
 		<tr>
 			<th>ID</th>
 			<th>Name</th>
 			<th>Operating System</th>
 			<th>IPv4</th>
 		</tr>
-
+		
 		<?php
 
-		// schakel warinings uit op de webpage
+		// schakel warinings uit op de webpage 
 		error_reporting(E_ERROR);
 		ini_set('display_errors', 1);
 
+
 		//Er worden variabelen gedefinieerd voor de servernaam, gebruikersnaam, wachtwoord en database-naam.
+		//include('config.php');
 		include('config.php');
+		
+	
 
-		// verbinden met database
-		$conn = mysqli_connect($config['servername'], $config['username'], $config['password'], $config['dbname']);
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
-   		 // Check connection
+if (mysqli_connect_errno()) {
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
+}
+
+   		// Check connection
    		 if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     	}
 
-  	  //Als er een POST-request is ontvangen en de parameter 'delete_id' is ingesteld, dan wordt het apparaat met de overeenkomende id verwijderd uit de database.
+  	    //Als er een POST-request is ontvangen en de parameter 'delete_id' is ingesteld, dan wordt het apparaat met de overeenkomende id verwijderd uit de database.
 		if(isset($_POST['delete_id'])) {
 			$delete_id = mysqli_real_escape_string($conn, $_POST['delete_id']);
 			$sql = "DELETE FROM device WHERE id='$delete_id'";
@@ -116,22 +123,22 @@
 			}
 		}
   
-    //Als er een POST-request is ontvangen, worden de invoervelden gevalideerd en wordt de data in de database ingevoegd als de invoer correct is.
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+     //Als er een POST-request is ontvangen, worden de invoervelden gevalideerd en wordt de data in de database ingevoegd als de invoer correct is.
+	 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    // Validate input data
-    $id = mysqli_real_escape_string($conn, $_POST["id"]);
-    $name = mysqli_real_escape_string($conn, $_POST["name"]);
-    $os = mysqli_real_escape_string($conn, $_POST["os"]);
-    $ipv4 = mysqli_real_escape_string($conn, $_POST["ipv4"]);
+     // Validate input data
+     $id = mysqli_real_escape_string($conn, $_POST["id"]);
+     $name = mysqli_real_escape_string($conn, $_POST["name"]);
+     $os = mysqli_real_escape_string($conn, $_POST["os"]);
+     $ipv4 = mysqli_real_escape_string($conn, $_POST["ipv4"]);
 
-    // check data
-    if (empty($id) || empty($name) || empty($os) || empty($ipv4)) {
+     // check data
+     if (empty($id) || empty($name) || empty($os) || empty($ipv4)) {
 		echo "<div class='error'>Please fill in all fields.</div>";
-	} elseif (!filter_var($ipv4, FILTER_VALIDATE_IP)) {
+	 } elseif (!filter_var($ipv4, FILTER_VALIDATE_IP)) {
 		echo "<div class='error'>Invalid IPv4 address.</div>";
-	} else {
-		// Use prepared statements to insert data into database
+	 } else {
+		// gebruik voor ingevoerde data om naar de database te zetten
 		$stmt = mysqli_prepare($conn, "INSERT INTO device (id, name, os, ipv4) VALUES (?, ?, ?, ?)");
 		mysqli_stmt_bind_param($stmt, "ssss", $id, $name, $os, $ipv4);
 
@@ -144,22 +151,15 @@
 		}
 
 		mysqli_stmt_close($stmt);
-	}
-}
-// Test error message
-$error_message = "This is a test error message.";
-echo "<div class='error'>" . $error_message . "</div>";
-error_log($error_message, 3, "error.log");
-file_put_contents('error.log', $error_message_test, FILE_APPEND);
+	 }
+	 }
+	
+     	// De data van alle apparaten wordt opgehaald uit de database en in een tabel weergegeven.
+     	$sql = "SELECT * FROM device";
+     $result = @mysqli_query($conn, $sql);
 
-
- 
-    // De data van alle apparaten wordt opgehaald uit de database en in een tabel weergegeven.
-    $sql = "SELECT * FROM device";
-    $result = @mysqli_query($conn, $sql);
-
-    // Elke rij in de tabel bevat een knop waarmee het bijbehorende apparaat kan worden verwijderd uit de database.
-    if (mysqli_num_rows($result) > 0) {
+     	// Elke rij in de tabel bevat een knop waarmee het bijbehorende apparaat kan worden verwijderd uit de database.
+     if (mysqli_num_rows($result) > 0) {
         while($row = mysqli_fetch_assoc($result)) {
             echo "<tr>";
             echo "<td>" . $row["id"] . "</td>";
@@ -174,7 +174,7 @@ file_put_contents('error.log', $error_message_test, FILE_APPEND);
             echo "</td>";
             echo "</tr>";
         }
-    } else {
+     } else {
         echo "<tr><td colspan='5'>0 results</td></tr>";
     }
 
